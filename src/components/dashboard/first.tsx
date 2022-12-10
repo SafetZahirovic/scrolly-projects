473f95ad7d78fcb482058118d43c95739ace2034
zahsaf17 <zahirovic.safet@gmail.com>
2022-12-10 23:23:59 +0100

    Changed dep image

--- src/components/dashboard/first.tsx
+++ src/components/dashboard/first.tsx
@@ -1,7 +1,7 @@
 export const DashboardFirstStep = () => {
   return (
     <div className="flex flex-col justify-evenly h-full">
-      <img src="dashboard-1.png"></img>
+      <img src="dp_tree.gif"></img>
       <img src="dashboard-2.png"></img>
     </div>
   );
